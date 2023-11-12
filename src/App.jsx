094807a5d46da0775372c6be44535819e7193fd5
Neil Vaca <neilvaca@outlook.com>
2023-11-11 19:10:37 -0500

    feature: add page not supported devices with width < 320

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './styles/App.css';
 
+import { NotSupported } from './pages/components/NotSupported';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import TermsOfUse from './pages/TermsOfUse';
 import Home from './pages/Home';
 import NoPage from './pages/NoPage';
 
 export const App = () => {
+  const [isSmallDevice, setIsSmallDevice] = useState(window.innerWidth < 300);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallDevice(window.innerWidth < 320);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  if (isSmallDevice) return <NotSupported />;
+
   return (
     <Router>
       <Routes>
---
+++ src/pages/components/NotSupported.jsx
@@ -0,0 +1,10 @@
+export const NotSupported = () => {
+  return (
+    <div className="not-supported">
+      <h1>Sorry, this website is not compatible with small devices</h1>
+      <hr />
+      <h2>Try to access with other devices to see </h2>
+      <a href="https://wiki-rickandmorty.web.app/">Wiki Rick and Morty</a>
+    </div>
+  );
+};
